27ad8ee5b2321902baa533b32366eab2dca05626
ashiun <83013095+ashiun@users.noreply.github.com>
2023-05-22 15:15:14 -0400

    Lab 2 complete

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const compression = require('compression');
+const passport = require('passport');
+const authenticate = require('./authentication');
 
 // version and author from our package.json file
 const { version, author } = require('../package.json');
@@ -64,6 +66,16 @@ app.use((err, req, res, next) => {
     },
   });
 });
+
+// Use gzip/deflate compression middleware
+app.use(compression());
+
+// Set up our passport authentication middleware
+passport.use(authenticate.strategy());
+app.use(passport.initialize());
+
+// Define our routes
+app.use('/', require('./routes'));
 
 // Export our `app` so we can access it in server.js
 module.exports = app;
